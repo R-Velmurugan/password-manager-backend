af53af701312ca2646aaebdb3612b99c234bde1d
vel <r.velmurugan1506@gmail.com>
2024-08-25 22:49:46 +0530

    removed unused imports

--- src/main/java/com/caput_draconis/query/Query.java
+++ src/main/java/com/caput_draconis/query/Query.java
@@ -1,18 +1,16 @@
 package com.caput_draconis.query;
 
 import com.caput_draconis.domain.domain.Password;
-import com.caput_draconis.domain.entity.PasswordEntity;
 import com.caput_draconis.service.PasswordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
 
 @Controller
 public class Query {
-    private PasswordService passwordService;
+    private final PasswordService passwordService;
 
     @Autowired
     public Query(PasswordService passwordService){
